6ec4aeb869044b2167a605a0e1a95f1288fd7874
Rayane Larbi <rayane06lesang@outlook.fr>
2024-11-05 17:41:17 +0100

    Update du ft_atoi

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: rlarbi <rlarbi@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 02:12:32 by rlarbi            #+#    #+#             */
-/*   Updated: 2024/10/30 04:01:58 by rlarbi           ###   ########.fr       */
+/*   Updated: 2024/11/05 17:39:09 by rlarbi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,42 @@ int	ft_atoi(const char *str)
 {
 	size_t	i;
 	int		res;
-	int		nb;
+	int		sign;
 
 	i = 0;
 	res = 0;
-	nb = 1;
+	sign = 1;
+	while (str[i] && (str[i] == ' ' || (str[i] <= 13 && str[i] >= 9)))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 48 && str[i] <= 57)
+			res = res * 10 + (str[i] - '0');
+		else
+			break ;
+		i++;
+	}
+	return (res * sign);
 }
+// int main(void)
+// {
+//     const char *test1 = "   -12345";
+//     const char *test2 = "42";
+//     const char *test3 = "   +00789";
+//     const char *test4 = "  \t\n\r\v\f123abc";
+//     const char *test5 = "abc123";
+
+//     printf("Test 1: %d\n", ft_atoi(test1));  // Devrait afficher -12345
+//     printf("Test 2: %d\n", ft_atoi(test2));  // Devrait afficher 42
+//     printf("Test 3: %d\n", ft_atoi(test3));  // Devrait afficher 789
+//     printf("Test 4: %d\n", ft_atoi(test4));  // Devrait afficher 123
+//     printf("Test 5: %d\n", ft_atoi(test5));  // Devrait afficher 0
+
+//     return (0);
+// }
