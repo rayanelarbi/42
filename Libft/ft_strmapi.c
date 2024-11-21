e753c85805b27ac263dde8b4cf0b3b2c3f4b7dc5
Rayane Larbi <rayane06lesang@outlook.fr>
2024-11-21 13:05:41 +0100

    Ajout de la fonction ft_strmapi

--- Libft/ft_strmapi.c
+++ Libft/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: rlarbi <rlarbi@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 12:15:37 by rlarbi            #+#    #+#             */
-/*   Updated: 2024/11/21 12:18:20 by rlarbi           ###   ########.fr       */
+/*   Updated: 2024/11/21 13:05:18 by rlarbi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s)) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
