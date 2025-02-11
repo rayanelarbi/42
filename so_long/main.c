3adc255aa1186e388c16886bccfec89105e5c7e6
Rayane Larbi <rlarbi@c1r2p4.42nice.fr>
2025-02-11 16:59:40 +0100

    Debut du projet so_long

---
+++ so_long/main.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlarbi <rlarbi@student.42nice.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 15:26:56 by rlarbi            #+#    #+#             */
+/*   Updated: 2025/02/11 16:58:52 by rlarbi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minilibx-linux/mlx.h"
+#define	WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1080
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void ft_put_pixel(t_data *data, int x, int y, int color)
+{
+    char *pxl;
+
+    if (x >= 0 && x < WINDOW_WIDTH && y >= 0 && y < WINDOW_HEIGHT)
+    {
+        pxl = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+        *(unsigned int *)pxl = color;
+    }
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	ft_put_pixel(&img, WINDOW_WIDTH/2, WINDOW_HEIGHT/2, 0xFF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+}
