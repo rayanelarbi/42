f68fc04c3edde60c2245c5b5750f0153a8f11098
Rayane Larbi <rlarbi@c1r2p4.42nice.fr>
2025-02-17 11:46:11 +0100

    Update du projet so long

--- so_long/main.c
+++ so_long/main.c
@@ -6,45 +6,34 @@
 /*   By: rlarbi <rlarbi@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 15:26:56 by rlarbi            #+#    #+#             */
-/*   Updated: 2025/02/11 16:58:52 by rlarbi           ###   ########.fr       */
+/*   Updated: 2025/02/17 11:27:11 by rlarbi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minilibx-linux/mlx.h"
-#define	WINDOW_WIDTH 1920
-#define WINDOW_HEIGHT 1080
+#include <unistd.h>
 
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
+void	*mlx_ptr;
+void	*win_ptr;
+void	*img_ptr;
+int width, height;
 
-void ft_put_pixel(t_data *data, int x, int y, int color)
+int	key_hook(int keycode, void *param)
 {
-    char *pxl;
-
-    if (x >= 0 && x < WINDOW_WIDTH && y >= 0 && y < WINDOW_HEIGHT)
-    {
-        pxl = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-        *(unsigned int *)pxl = color;
-    }
+	(void)param;          // Empêche le warning "unused parameter"
+	if (keycode == 65307) // ESC
+		mlx_destroy_window(mlx_ptr, win_ptr);
+	return (0);
 }
 
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
-
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-	ft_put_pixel(&img, WINDOW_WIDTH/2, WINDOW_HEIGHT/2, 0xFF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 800, 600, "So Long");
+	img_ptr = mlx_xpm_file_to_image(mlx_ptr, "./textures/steve.xpm", &width,
+			&height);
+	mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 100, 100);
+	mlx_key_hook(win_ptr, key_hook, NULL);
+	mlx_loop(mlx_ptr);
+	return (0);
 }
