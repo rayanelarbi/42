d18cacf8306de0a890d102fd3fae440337a9e0d8
Rayane Larbi <rayane06lesang@outlook.fr>
2025-03-19 10:53:28 +0100

    Début du projet push_swap

---
+++ push_swap/include/push_swap.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlarbi <rlarbi@student.42nice.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/18 23:57:30 by rlarbi            #+#    #+#             */
+/*   Updated: 2025/03/19 10:52:53 by rlarbi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <stdbool.h>
+
+
+
+#endif
